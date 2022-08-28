4a727832d0253fe8909d80d06f5e5341e778f742
darshanrm <darshan.marathe13@gmail.com>
2022-08-28 16:58:23 +0530

    Gateway server and resilliency

--- accounts/accounts/src/main/java/com/bankapplication/accounts/client/CardsFeignClient.java
+++ accounts/accounts/src/main/java/com/bankapplication/accounts/client/CardsFeignClient.java
@@ -4,6 +4,7 @@ import com.bankapplication.accounts.model.Cards;
 import com.bankapplication.accounts.model.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.List;
 @FeignClient("CARDS")
 public interface CardsFeignClient {
     @RequestMapping(method = RequestMethod.POST,value = "cards",consumes = "application/json")
-    List<Cards> getCardDetails(@RequestBody Customer customer);
+    List<Cards> getCardDetails(@RequestHeader("bank-correlation-id") String correlationid, @RequestBody Customer customer);
 }
--- accounts/accounts/src/main/java/com/bankapplication/accounts/client/LoansFeignClient.java
+++ accounts/accounts/src/main/java/com/bankapplication/accounts/client/LoansFeignClient.java
@@ -6,6 +6,7 @@ import com.bankapplication.accounts.model.Customer;
 import com.bankapplication.accounts.model.Loans;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,5 +14,5 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface LoansFeignClient {
 
     @RequestMapping(method = RequestMethod.POST, value = "loans", consumes = "application/json")
-    List<Loans> getLoansDetails(@RequestBody Customer customer);
+    List<Loans> getLoansDetails(@RequestHeader("bank-correlation-id") String correlationid, @RequestBody Customer customer);
 }
